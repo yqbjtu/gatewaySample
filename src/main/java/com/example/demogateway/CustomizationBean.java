9480117f819181802bb80fd7b28e34bb06567c53
yqbjtu <yqbjtu@163.com>
2018-08-08 19:46:59 +0800

    my gateway with oauth

--- src/main/java/com/example/demogateway/CustomizationBean.java
+++ src/main/java/com/example/demogateway/CustomizationBean.java
@@ -1,29 +1,29 @@
-package com.example.demogateway;
-
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.ErrorPage;
-import org.springframework.boot.web.server.WebServerFactoryCustomizer;
-import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Component;
-
-import java.util.concurrent.TimeUnit;
-
-@Component
-public class CustomizationBean implements WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> {
-
-    @Override
-    public void customize(ConfigurableServletWebServerFactory server) {
-        server.setPort(9000);
-    }
-
-    @Bean
-    public ConfigurableServletWebServerFactory webServerFactory() {
-        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
-        factory.setPort(9000);
-        //factory.setSessionTimeout(10, TimeUnit.MINUTES);
-        factory.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/notfound.html"));
-        return factory;
-    }
-}
+//package com.example.demogateway;
+//
+//import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+//import org.springframework.boot.web.server.ErrorPage;
+//import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+//import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.stereotype.Component;
+//
+//import java.util.concurrent.TimeUnit;
+//
+//@Component
+//public class CustomizationBean implements WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> {
+//
+//    @Override
+//    public void customize(ConfigurableServletWebServerFactory server) {
+//        server.setPort(9000);
+//    }
+//
+//    @Bean
+//    public ConfigurableServletWebServerFactory webServerFactory() {
+//        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+//        factory.setPort(9000);
+//        //factory.setSessionTimeout(10, TimeUnit.MINUTES);
+//        factory.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/notfound.html"));
+//        return factory;
+//    }
+//}
